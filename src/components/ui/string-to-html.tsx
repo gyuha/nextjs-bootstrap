5c0ac5170c76dc45b91e8cfa4e29ba6d94ba7f62
gyuha <nicegyuha@gmail.com>
2026-04-20 22:26:04 +0900

    feat: add modal dependencies and utility functions

---
+++ src/components/ui/string-to-html.tsx
@@ -0,0 +1,19 @@
+import DOMPurify from "dompurify";
+import type React from "react";
+
+interface IStringToHtmlProps {
+	text: string | React.ReactNode | React.JSX.Element | null;
+}
+
+const StringToHtml = ({
+	text,
+}: IStringToHtmlProps): React.ReactNode | React.JSX.Element | null => {
+	if (typeof text === "string") {
+		const sanitizedData = DOMPurify.sanitize(text);
+		// biome-ignore lint/security/noDangerouslySetInnerHtml: Content is sanitized with DOMPurify
+		return <div dangerouslySetInnerHTML={{ __html: sanitizedData }} />;
+	}
+	return text;
+};
+
+export default StringToHtml;
